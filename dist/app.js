184ce6a86766c319a794ae6dbb35fe6e38d65ef5
Aleksei <amashuroff@gmail.com>
2020-12-02 15:07:00 +0300

    interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,79 +1,25 @@
 "use strict";
-// public private protected - available only in TS
-// abstract class , abstract method / property use when want to enforce all inheriting classes to have
-// a common property / method
-// inheriting class should provide the returned value / implement this method, prop
-// abstract classes can't be instantiated themselves
-// singleton pattern ensures that only one instance of the certain class is created
-class Department {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        // private properties are only accessible from inside the class where they are defined
-        // use protected if want to access in every class that extends this.
-        this.employees = []; // private property only accessible inside the class
-        // shortcut --> pass props directly into constructor without initialization
-    }
-    static logSomething() {
-        console.log("hello world");
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    seeEmployees() {
-        console.log(this.employees);
-    }
-}
-class ITDepartment extends Department {
-    constructor(id) {
-        super("IT", id);
-        this.admins = [];
-        this.hiddenWord = "hidden word";
-    }
-    get hidden() {
-        // getter always should return something
-        if (!this.hiddenWord)
-            throw new Error("No hidden word found");
-        return this.hiddenWord;
-    }
-    set hidden(value) {
-        if (!value)
-            throw new Error("Please check the input");
-        this.hiddenWord = value;
-    }
-    static getInstance() {
-        if (this.instance) {
-            return this.instance;
+// INTERFACE
+// allows to define structure of an object
+let user1;
+user1 = {
+    name: "Alex",
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    },
+};
+class Person {
+    constructor(n) {
+        // Greetable, Named
+        // can have more methods, but should implement all props of the Greetable interface
+        this.age = 30;
+        if (n) {
+            this.name = n;
         }
-        this.instance = new ITDepartment(139);
-        return this.instance;
     }
-    describe() {
-        console.log("This is ITDepartment: " + this.id);
-    }
-    addAdmin(admin) {
-        this.admins.push(admin);
-    }
-    showAdmins() {
-        console.log(this.admins);
-    }
-    addEmployee(name) {
-        if (name === "Alex")
-            return;
-        this.employees.push(name);
+    greet(phrase) {
+        console.log(phrase + this.name);
     }
 }
-// const accounting = new Department( 12); --> cannot create instance of abstract class
-const it = ITDepartment.getInstance();
-// accounting.describe();
-// accounting.addEmployee("John");
-// accounting.seeEmployees();
-// const it = new ITDepartment(133);
-// it.addEmployee("Alex");
-// it.addEmployee("Li");
-// it.seeEmployees();
-// it.addAdmin("Max");
-// it.showAdmins();
-// console.log(it.hidden); // getter
-// it.hidden = "i am the new hidden word";
-// console.log(it.hidden);
+const user2 = new Person("Maria"); // can create with or without a name
+const add2nums = (a, b) => a + b;
